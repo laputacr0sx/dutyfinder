f512929d471145f6da50dc020263b12b30d104df
Veilics <>
2023-03-03 11:30:04 +0800

    minor changes of result Page, styling not working

--- src/component/ShiftsDetail.js
+++ src/component/ShiftsDetail.js
@@ -6,34 +6,32 @@ function ShiftsDetail({shiftObject}) {
 
   return (<>
 
-        <p className={''} color={'red'}>{dutyNumber}</p>
 
-
-        {/*<table*/}
-        {/*    className="table-fixed border border-zinc-800 border-collapse border-spacing-2">*/}
-        {/*  <thead>*/}
-        {/*  <tr>*/}
-        {/*    <th>DutyName</th>*/}
-        {/*    <th>bookOnLocation</th>*/}
-        {/*    <th>bookOnTime</th>*/}
-        {/*    <th>bookOffTime</th>*/}
-        {/*    <th>bookOffLocation</th>*/}
-        {/*    <th>duration</th>*/}
-        {/*    <th>remarks</th>*/}
-        {/*  </tr>*/}
-        {/*  </thead>*/}
-        {/*  <tbody>*/}
-        {/*  <tr align={'center'}>*/}
-        {/*    <td>{dutyNumber}</td>*/}
-        {/*    <td>{bNL}</td>*/}
-        {/*    <td>{bNT}</td>*/}
-        {/*    <td>{bFT}</td>*/}
-        {/*    <td>{bFL}</td>*/}
-        {/*    <td>{duration}</td>*/}
-        {/*    <td>{remarks}</td>*/}
-        {/*  </tr>*/}
-        {/*  </tbody>*/}
-        {/*</table>*/}
+        <table
+            className="table-fixed border border-zinc-800 border-collapse border-spacing-2">
+          <thead>
+          <tr>
+            <th>DutyName</th>
+            <th>bookOnLocation</th>
+            <th>bookOnTime</th>
+            <th>bookOffTime</th>
+            <th>bookOffLocation</th>
+            <th>duration</th>
+            <th>remarks</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr align={'center'}>
+            <td>{dutyNumber}</td>
+            <td>{bNL}</td>
+            <td>{bNT}</td>
+            <td>{bFT}</td>
+            <td>{bFL}</td>
+            <td>{duration}</td>
+            <td>{remarks}</td>
+          </tr>
+          </tbody>
+        </table>
       </>
 
   );
